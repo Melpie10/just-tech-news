6ddfbddbd7fd212fec60894a249f4d483f344867
melany14 <melany14@gmail.com>
2022-02-06 16:54:39 -0500

    added bcrypt hooks

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -55,11 +55,13 @@ router.post('/', (req, res) => {
 router.put('/:id', (req, res) => {
   // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
 
-  // if req.body has exact key/value pairs to match the model, you can just use `req.body` instead
+  // pass in req.body instead to only update what's passed through
   User.update(req.body, {
+    individualHooks: true,
     where: {
       id: req.params.id
     }
+
   })
     .then(dbUserData => {
       if (!dbUserData[0]) {
@@ -76,22 +78,22 @@ router.put('/:id', (req, res) => {
 
 // DELETE /api/users/1
 router.delete('/:id', (req, res) => {
-    User.destroy({
-        where: {
-          id: req.params.id
+  User.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No user found with this id' });
+          return;
         }
+        res.json(dbUserData);
       })
-        .then(dbUserData => {
-          if (!dbUserData) {
-            res.status(404).json({ message: 'No user found with this id' });
-            return;
-          }
-          res.json(dbUserData);
-        })
-        .catch(err => {
-          console.log(err);
-          res.status(500).json(err);
-        });
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
 });
 
 module.exports = router;
