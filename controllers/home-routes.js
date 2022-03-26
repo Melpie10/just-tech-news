b552cc617d6b468c13d28825e43a6b4b0fee4d71
melany14 <melany14@gmail.com>
2022-03-26 11:33:29 -0400

    added dashboard handlebars

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,7 +3,9 @@ const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 
 
+// get all posts for homepage
 router.get('/', (req, res) => {
+  console.log('======================');
   Post.findAll({
     attributes: [
       'id',
@@ -28,9 +30,9 @@ router.get('/', (req, res) => {
     ]
   })
     .then(dbPostData => {
-      // pass a single post object into the homepage template
-      console.log(dbPostData[0]);
-      res.render('homepage', dbPostData[0]);
+      const posts = dbPostData.map(post => post.get({ plain: true }));
+
+      res.render('homepage', { posts });
     })
     .catch(err => {
       console.log(err);
